42fd623adfabedeeef7f6cf3a87fb4912a7aea85
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-04-28 22:17:46 +0700

    make interface constexpr

--- 00-lang-tag-dispatch/include/elements.hpp
+++ 00-lang-tag-dispatch/include/elements.hpp
@@ -13,47 +13,47 @@ struct choice<0> {};
 namespace elements {
     namespace detail {
         template <typename T1, size_t order=3>
-        auto x(choice<order>, T1& p) -> decltype((p.x)) {
+        constexpr auto x(choice<order>, T1& p) -> decltype((p.x)) {
             return p.x;
         }
         template <typename T1, size_t order=2>
-        auto x(choice<order>, T1& p) -> decltype((p.x())) {
+        constexpr auto x(choice<order>, T1& p) -> decltype((p.x())) {
             return p.x();
         }
         template <typename T1, size_t order=1>
-        auto x(choice<order>, T1& p) -> decltype((p(0))) {
+        constexpr auto x(choice<order>, T1& p) -> decltype((p(0))) {
             return p(0);
         }
         template <typename T1, size_t order=0>
-        auto x(choice<order>, T1& p) -> decltype((p[0])) {
+        constexpr auto x(choice<order>, T1& p) -> decltype((p[0])) {
             return p[0];
         }
         // const overload
         template <typename T1, size_t order=3>
-        const auto x(choice<order>, const T1& p) -> decltype(p.x) {
+        constexpr const auto x(choice<order>, const T1& p) -> decltype(p.x) {
             return p.x;
         }
         template <typename T1, size_t order=2>
-        const auto x(choice<order>, const T1& p) -> decltype(p.x()) {
+        constexpr const auto x(choice<order>, const T1& p) -> decltype(p.x()) {
             return p.x();
         }
         template <typename T1, size_t order=1>
-        const auto x(choice<order>, const T1& p) -> decltype(p(0)) {
+        constexpr const auto x(choice<order>, const T1& p) -> decltype(p(0)) {
             return p(0);
         }
         template <typename T1, size_t order=0>
-        const auto x(choice<order>, const T1& p) -> decltype(p[0]) {
+        constexpr const auto x(choice<order>, const T1& p) -> decltype(p[0]) {
             return p[0];
         }
     }
     // entry point for x
     template <typename T, size_t n_choice=3>
-    auto x(T& p) -> decltype((detail::x<T>(choice<n_choice>{},p)))
+    constexpr auto x(T& p) -> decltype((detail::x<T>(choice<n_choice>{},p)))
     {
         return detail::x(choice<n_choice>{},p);
     }
     template <typename T, size_t n_choice=3>
-    const auto x(const T& p) -> decltype((detail::x<T>(choice<n_choice>{},p)))
+    constexpr const auto x(const T& p) -> decltype((detail::x<T>(choice<n_choice>{},p)))
     {
         return detail::x(choice<n_choice>{},p);
     }
