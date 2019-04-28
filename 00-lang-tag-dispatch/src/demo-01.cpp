a58dc1c7ce2a2e3185a0fd6f19f48eb013bae722
Fahri Ali Rahman <ali.rahman.fahri@gmail.com>
2019-04-28 22:18:13 +0700

    add pointer

--- 00-lang-tag-dispatch/src/demo-01.cpp
+++ 00-lang-tag-dispatch/src/demo-01.cpp
@@ -4,18 +4,27 @@
 #include <cmath>
 #include <array>
 #include <vector>
+#include <random>
 
 #define print(EXPR) std::cout << #EXPR << " : " << EXPR << std::endl;
 
 namespace elm = elements;
 
+auto f() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(1.0,2.0);
+    return dis(gen);
+}
+
 int main(int argc, char**argv)
 {
+    /* prepare placeholder */
     Point1<double> p1;
     Point2<double> p2;
     Point3<double> p3;
     Point4<double> p4;
-
+    auto p1_ptr = new Point1<double>();
     double raw_point[2];
     std::vector<double> vector_point(5);
     std::array<double,7> array_point;
@@ -28,7 +37,10 @@ int main(int argc, char**argv)
     print(elm::x(raw_point));
     print(elm::x(vector_point));
     print(elm::x(array_point));
+    print(elm::x(*p1_ptr));
 
+    /* do something with the container */
+    /* note that we could do operation across different types */
     elm::x(p1) = M_PI;
     elm::x(p2) = elm::x(p1);
     elm::x(p3) = elm::x(p1) * elm::x(p2);
@@ -36,6 +48,7 @@ int main(int argc, char**argv)
     elm::x(raw_point) = elm::x(p4) / (-elm::x(p4));
     elm::x(vector_point) = elm::x(raw_point) + M_PI;
     elm::x(array_point) = elm::x(vector_point) - elm::x(p1);
+    elm::x(*p1_ptr) = (elm::x(p1) + elm::x(p4)) / (elm::x(p3) + M_PI + 2);
 
     /* check values for each point type */
     print(elm::x(p1));
@@ -45,6 +58,11 @@ int main(int argc, char**argv)
     print(elm::x(raw_point));
     print(elm::x(vector_point));
     print(elm::x(array_point));
+    print(elm::x(*p1_ptr));
+
+    /* some random values */
+    elm::x(p1) = f();
+    print(elm::x(p1));
 
     return 0;
 }
